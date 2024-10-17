7922e5c3882449a16a22ce6fd997282d42f9b8e6
webQbe <webcubesolutionslk@gmail.com>
2024-10-17 19:14:24 +0530

    Complied async function & logged API data

--- src/app.js
+++ src/app.js
@@ -8,3 +8,16 @@ console.log(person.name);
 
 // log function output
 console.log(sayHello('Ishan'));
+
+// async function with fetch api
+async function getPosts(){
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    
+    const data = await response.json();
+    return data;
+
+}
+
+// call getPosts()
+getPosts().then(posts => console.log(posts));
